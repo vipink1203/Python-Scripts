4f6fc0da2f266d677c6af59a3140923eb459e43e
Vipin Kumar <vkumar@AHMB100041-VipinKumar.local>
2019-10-28 12:24:10 +0530

    updated header

--- kms-decrypt.py
+++ kms-decrypt.py
@@ -1,3 +1,9 @@
+__author__ = "Vipin Kumar"
+__version__ = "0.0.1"
+__maintainer__ = "Vipin Kumar"
+__status__ = "Production"
+
+
 import base64
 import boto3
 
@@ -6,9 +12,10 @@ def decrypt_key():
     with open("EncryptedFile", "rb") as f:
         byte = f.read()
         resp = kms.decrypt(CiphertextBlob=byte)
-        plainKey = resp['Plaintext']
-        final_key = plainKey.decode('UTF-8')
-        return final_key
+        f.close()
+    plainKey = resp['Plaintext']
+    final_key = plainKey.decode('UTF-8')
+    return final_key
 
 ssh_key = decrypt_key()
 print(ssh_key)
